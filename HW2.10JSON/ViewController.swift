00df29ad3dbd1e13308d8cbb366b0d68abe5eeb2
Alex Sobolev <31108@mail.ru>
2022-09-04 15:34:35 +0300

    Initial Commit

---
+++ HW2.10JSON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.10JSON
+//
+//  Created by Александр Соболев on 04.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
