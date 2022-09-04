d5372186b75ac283be299afb5151f1cd21c2d9b2
Alex Sobolev <31108@mail.ru>
2022-09-04 15:57:55 +0300

    Setup model

--- HW2.10JSON/Models/SW.swift
+++ HW2.10JSON/Models/SW.swift
@@ -5,4 +5,19 @@
 //  Created by Александр Соболев on 04.09.2022.
 //
 
-import Foundation
+struct WebsiteDescription: Decodable {
+    let climate: String?
+    let diameter: String?
+    let name: String?
+    let orbital_period: String?
+    let population: String?
+    let residents: [String]?
+    let rotation: String?
+    let surface: String?
+    let terrain: String?
+    let url: String?
+}
+
+enum Link: String {
+    case starWars = "https://swapi.dev/api/"
+}
